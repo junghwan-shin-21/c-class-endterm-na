dc6558fcf628ed3bf95ea866bd54f52bd4eb6c31
junghwan-shin-21 <junghwan.shin@ssu.ac.kr>
2025-12-07 01:53:06 +0900

    Add function prototypes for file operations

--- main-2.c
+++ main-2.c
@@ -1,3 +1,8 @@
+//사용 함수
+FILE *fopen(const char *filename, const char *mode);
+int fprintf(FILE *fp, const char *format, ...);
+int fscanf(FILE *fp, const char *format, ...);
+
 #include <stdio.h>
 
 int main(void)  {
